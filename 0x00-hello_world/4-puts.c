5fe1c7f6390fd22ea24f90d8cc357a8cb1146e59
adetariku@gmail.com <adetariku@gmail.com>
2022-07-01 07:38:40 +0900

    output string fixed

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -10,7 +10,7 @@
 
 int main(void){
 
-   puts("\"Programing is like building a multilingual puzzle, followed by a new line");
+   puts("\"Programing is like building a multilingual puzzle");
   
     return 0;
 
