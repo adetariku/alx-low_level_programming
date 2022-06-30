bab1c26bd5153832ffe0d4f95fcef215deeb1679
adetariku@gmail.com <adetariku@gmail.com>
2022-07-01 07:36:30 +0900

    a C program that prints exactly a given string

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *
+ * main -entry point
+ * 
+ * Return always 0(success)
+ *
+ *
+*/
+
+int main(void){
+
+   puts("\"Programing is like building a multilingual puzzle, followed by a new line");
+  
+    return 0;
+
+}
