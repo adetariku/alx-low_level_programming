6c24a826333f848a5921f8e5867bc6b26d147a74
adetariku@gmail.com <adetariku@gmail.com>
2022-07-02 11:52:17 +0900

    directory was placed wrongly

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+nclude <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int d;
+
+	for (d = '0'; d <= '9'; d++)
+		putchar(d);
+		putchar('\n');
+
+	return (0);
+}
