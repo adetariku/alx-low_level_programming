0e61a081b64f20c9bd343ad4c6c38cb5668eacfa
Tariku Wendimagegn <adetariku@gmail.com>
2022-07-03 00:27:02 -0700

    fix Bety style indentation

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,13 @@
 #include <stdio.h>
 /**
  * main -entry point
- * 
- * Return always 0(success)
+ *
+ * Return: always 0 (success)
  */
-
 int main(void)
 {
+	puts("\"Programming is like building a multilingual puzzle");
 
-	puts("\"Programming is like building a multilingual puzzle\n");
-
-	return 0;
-
+	return (0);
 
 }
