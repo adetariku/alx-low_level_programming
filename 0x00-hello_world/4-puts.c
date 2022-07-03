8311968e611c3567ca3964d75dee2656906352ed
Tariku Wendimagegn <adetariku@gmail.com>
2022-07-03 00:18:12 -0700

    fix Betty style coding

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,12 +3,14 @@
  * main -entry point
  * 
  * Return always 0(success)
-*/
+ */
 
-int main(void){
+int main(void)
+{
 
-   puts("\"Programming is like building a multilingual puzzle");
-  
-    return 0;
+	puts("\"Programming is like building a multilingual puzzle\n");
+
+	return 0;
+
 
 }
