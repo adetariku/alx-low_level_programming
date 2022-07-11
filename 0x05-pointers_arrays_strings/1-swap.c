3c5fbd262639eecfe2547779c7fbb663b42ef886
adetariku@gmail.com <adetariku@gmail.com>
2022-07-12 01:02:14 +0900

    swap integers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+/**
+  *swap_int - swaps the values of two integer variables
+  * @a : the first variable
+  * @b : the second variable
+  * 
+  * Return - void
+  */
+void swap_int(int *a, int *b)
+{
+	int c;
+	
+	c = (int)*a;
+	*a = *b;
+	*b = c;
+}
+  
