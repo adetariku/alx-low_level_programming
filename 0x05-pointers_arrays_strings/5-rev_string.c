40cb529c82baac8abe9b6c6fe3461ef3b79d975f
Tariku Wendimagegn <adetariku@gmail.com>
2022-07-11 21:35:59 -0700

    reversing a strig

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,24 +1,32 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * print_rev - print a string in reverse order..
+ * rev_string - print a string in reverse order..
  * @s: pointer int type
+ *
+ * Description - reverses a given string.
  * Return: Always 0.
  */
-void print_rev(char *s)
+void rev_string(char *s)
 {
 	int len = 0;
+	char *temp;
+
 
 	while (*s != '\0')
 	{
 		s++;
 		len++;
 	}
-	
-	 while (len >0)
-	 {
-		_putchar(*(--s));
-               len--;
-	 }		
+	s = s - 1;
+	while (len > 0)
+	{
+		temp = s;
+		_putchar(*temp);
+		s--;
+		temp++;
+		len--;
+	}
+	*s = *temp;
 	_putchar('\n');
 }
