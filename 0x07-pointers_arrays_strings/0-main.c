416837a72b430160574d42b9dbf8cc53ba5de8c5
Tariku Wendimagegn <adetariku@gmail.com>
2022-07-18 17:37:46 -0700

    fill n locations of a memory buffer with a hexadecimal

--- 0x07-pointers_arrays_strings/0-main.c
+++ 0x07-pointers_arrays_strings/0-main.c
@@ -38,8 +38,9 @@ int main(void)
 {
     char buffer[98] = {0x00};
 
+    printf("before buffer is trhf aljfd ;fjkfa  %p",buffer);
     simple_print_buffer(buffer, 98);
-    _memset(buffer, 0x01, 95);
+    _memset(buffer, 0x03, 95);
     printf("-------------------------------------------------\n");
     simple_print_buffer(buffer, 98);    
     return (0);
