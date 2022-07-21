64fbd7e7268b65e3c19d397a369b857a43033701
Tariku Wendimagegn <adetariku@gmail.com>
2022-07-21 10:13:23 -0700

    print reverse string

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,11 +1,12 @@
+#include <stdio.h>
 #include "main.h"
 /**
-  * _print_rev_recursion - see the description.
-  * @s : a character pointer
-  *
-  * Description : Prints a string (character pointer) in a reverese order
-  * Return : void (Nothing)
-  */
+ * _print_rev_recursion - see the description.
+ * @s : a character pointer
+ *
+ *  Description : Prints a string (character pointer) in a reverese order
+ *  Return : void (Nothing)
+ */
 void _print_rev_recursion(char *s)
 {
 	if (*s == '\0')
