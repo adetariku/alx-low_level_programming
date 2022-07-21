ba4d81914a34fb0561696d89d147863e699ae468
Tariku Wendimagegn <adetariku@gmail.com>
2022-07-21 12:07:20 -0700

    cpmputes the natural square root

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * square - checks if a number has a square
+ * @i: counter
+ * @n: input number n
+ *
+ * Return: -1 or factor
+ */
+int square(int n, int i)
+{
+	if (i > n)
+	{
+		return (-1);
+	}
+	if (n == i * i)
+	{
+		return (i);
+	}
+	return (square(n, i + 1));
+}
+/**
+ * _square_root - find a natural quare root
+ * @n: the input number
+ *
+ * Return: -1 if n has no natural root, else the natural square root
+ */
+int _sqrt_recursion(int n)
+{
+	int counter = 1;
+
+	return (square(n, counter));
+}
