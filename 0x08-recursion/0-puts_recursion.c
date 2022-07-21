065e38cad590739ff1233c46ea263eeb2ba9f30f
Tariku Wendimagegn <adetariku@gmail.com>
2022-07-21 07:53:58 -0700

    print a string in reverse

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,13 +1,14 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * _puts_recursion - prints a character pointer using recursive function
- * @s - a character pointer
- * 
+ * @s: a character pointer
+ *
  * Return : Nothing(void)
  */
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
+	if(*s == '\0')
 	{
 		_putchar('\n');
 		return;
