f3fae6e85a3ef45e1bb18f48af0ba3a800a2343a
Tariku Wendimagegn <adetariku@gmail.com>
2022-07-26 09:44:22 -0700

    declear a 2 dimensional array

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * alloc_grid - returns a pointer to a 2D array
+ * @width: width input
+ * @height: height input
+ *
+ * Return: pointer to 2 dim. array
+ */
+int **alloc_grid(int width, int height)
+{
+	int **pointer_to_2d;
+	int index = 0, col;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+	pointer_to_2d = malloc(sizeof(int) * height);
+	if (pointer_to_2d == NULL)
+	{
+		free(pointer_to_2d);
+		return (NULL);
+	}
+	while (index < height)
+	{
+		pointer_to_2d[index] = malloc(sizeof(int) * width);
+		if (pointer_to_2d == NULL)
+		{
+			free(pointer_to_2d);
+			return (NULL);
+		}
+		for (col = 0; col < width; col++)
+		{
+			pointer_to_2d[index][col] = 0;
+		}
+		index++;
+	}
+	return (pointer_to_2d);
+}
