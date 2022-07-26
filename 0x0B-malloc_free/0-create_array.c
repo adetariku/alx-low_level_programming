c48ae0a0bcfcff1e5649999daef6e60553662656
Tariku Wendimagegn <adetariku@gmail.com>
2022-07-26 05:55:14 -0700

    correcting allocated space size

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,7 +15,7 @@ char *create_array(unsigned int size, char c)
 
 	if (size < 1)
 		return (pointer);
-	pointer = malloc(size * sizeof(unsigned int));
+	pointer = malloc(size * sizeof(char));
 	if (pointer == NULL)
 		return (NULL);
 	for (index = 0; index < size; index++)
