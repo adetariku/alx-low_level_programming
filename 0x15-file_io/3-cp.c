d92036f9b790b2a8963f52c2bc2305cdcb035c35
Tariku Wendimagegn <adetariku@gmail.com>
2022-08-22 19:19:18 -0700

    copies content of a file to another file

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,63 @@
+#include "main.h"
+/**
+  * printerrors - prints errors
+  * @message:  message to be printed
+  * @file: destination filename
+  * @exitVal: exit status
+  *
+  * Return: void(nothing)
+  */
+void printerrors(char *message, char *file, int exitVal)
+{
+	dprintf(STDERR_FILENO, "%s%s\n", message, file);
+	exit(exitVal);
+}
+/**
+  * main - copies a file
+  * @argc: number of args
+  * @argv: pointer to array of args.
+  *
+  * Return: 0
+  */
+int main(int argc, char **argv)
+{
+	int fddest;
+	int fdsrc;
+	int writeVal;
+	int readVal;
+	char buffer[1024];
+
+	if (argc != 3)
+		printerrors("Usage: cp file_from file_to", "", 97);
+
+	fddest = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (fddest == -1)
+		printerrors("Error: Can't write to ", argv[2], 99);
+
+	fdsrc = open(argv[1], O_RDONLY);
+	if (fdsrc == -1)
+		printerrors("Error: Can't read from file ", argv[1], 98);
+
+
+	do {
+		readVal = read(fdsrc, buffer, 1024);
+		if (readVal == -1)
+			printerrors("Error: Can't read from file ", argv[1], 98);
+
+		writeVal = write(fddest, buffer, readVal);
+		if (writeVal == -1 || writeVal != readVal)
+			printerrors("Error: Can't write to ", argv[2], 99);
+
+		} while (writeVal == 1024);
+	if (close(fdsrc))
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fdsrc);
+		exit(100);
+	}
+	if (close(fddest))
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fddest);
+		exit(100);
+	}
+	return (0);
+}
