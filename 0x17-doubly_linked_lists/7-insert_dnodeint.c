0ae3ed8d8689ed874b0f3539a7303dbcab5bfd89
Tariku Wendimagegn <adetariku@gmail.com>
2022-10-01 01:52:48 +0000

    inserta new node

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -3,24 +3,24 @@
 #include <stdio.h>
 
 /**
- * insert_dnodeint_at_index - inserts a new node at a given position
- * @h: double pointer to the beginning of the linked list
+ * insert_dnodeint_at_index - inserts a node at a given position
+ * @h: double ptr to the start of the linked list
  * @idx: index at which to insert the new node
  * @n: data to enter into new node
  *
- * Return: pointer to the new node, or NULL on failure
+ * Return: ptr to the new node, or NULL on failure
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *new, *next, *current;
-	unsigned int i;
+	unsigned int indx;
 
 	if (h == NULL)
 		return (NULL);
 	if (idx != 0)
 	{
 		current = *h;
-		for (i = 0; i < idx - 1 && current != NULL; i++)
+		for (indx = 0; indx < idx - 1 && current != NULL; indx++)
 			current = current->next;
 		if (current == NULL)
 			return (NULL);
