4294862cc16cab55dc9e9558650a693f5d956432
Wendimagegn Tariku <adetariku@gmail.com>
2022-12-08 06:24:50 -0800

    fix to betty style

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
  * mul - computes the product of two numbers
- * @a : the first number
- * @b : the second number
+ * @a: the first number
+ * @b: the second number
  *
- * Return : the product of a and b
+ * Return: the product of a and b
  */
 int mul(int a, int b)
 {
