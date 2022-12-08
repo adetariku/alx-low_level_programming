317a444a6d842ecad239654a4cabf6562a57c2be
Wendimagegn Tariku <adetariku@gmail.com>
2022-12-08 06:11:34 -0800

    betty style

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,9 +1,10 @@
 #include "main.h"
 /**
  * _isdigit - prints if a character is a digit or not.
- * @c - input character
+ * @c: - input character
  *
- * Return : 1 if character is digit , 0 otherwise.
+ * Description: checks if a character is upper
+ * Return: 1 if character is digit , 0 otherwise.
  */
 int _isdigit(int c)
 {
