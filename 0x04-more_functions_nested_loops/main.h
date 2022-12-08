b9e73b253da3567f2d1742dc42051569263dd4e8
Wendimagegn Tariku <adetariku@gmail.com>
2022-12-08 06:56:10 -0800

    emm..

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,7 +1,7 @@
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int , int);
-void print_numbers();
-void print_most_numbers();
-void more_numbers();
-int _putchar(int c);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void _putchar(int c);
